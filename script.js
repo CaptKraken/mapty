20cdee14ea317490c8903e5958d0784569a2b626
Song Kim <kimsong432@gmail.com>
2021-06-06 16:42:35 +0700

    set map click event

--- script.js
+++ script.js
@@ -21,11 +21,22 @@ navigator.geolocation.getCurrentPosition(
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-
-    L.marker(latLong)
-      .addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    map.on('click', mapEvent => {
+      const { latlng } = mapEvent;
+      console.log(mapEvent);
+      L.marker(latlng)
+        .addTo(map)
+        .bindPopup(
+          L.popup({
+            maxWidth: 250,
+            minWidth: 100,
+            autoClose: false,
+            closeOnClick: false,
+            className: 'running-popup',
+          })
+        )
+        .openPopup();
+    });
   },
   () => {
     popupMessage('couldnt get your position');
